ca5e016d152fc8fcfef6eec398476c1a2499054a
Vlad <alzwded@gmail.com>
2014-11-10 20:15:45 +0200

    Fix #2
    bump version

--- AltTabber/AltTabber.cpp
+++ AltTabber/AltTabber.cpp
@@ -258,6 +258,20 @@ static MonitorGeom_t GetMonitorGeometry()
     return ret;
 }
 
+
+static void MoveCursorOverActiveSlot()
+{
+    if(g_programState.activeSlot < 0) return;
+
+    auto& r = g_programState.slots[g_programState.activeSlot].r;
+    POINT moveHere = {
+        (r.right + r.left) / 2,
+        (r.bottom + r.top) / 2,
+    };
+    ClientToScreen(g_programState.hWnd, &moveHere);
+    SetCursorPos(moveHere.x, moveHere.y);
+}
+
 void SynchronizeWithRegistry()
 {
 #define SUBKEY (_T("Software\\jakkal\\AltTabber"))
@@ -835,6 +849,9 @@ static void MoveNext(DWORD direction)
                 g_programState.activeSlot,
                 (void*)g_programState.slots[g_programState.activeSlot].hwnd);
     }
+
+    MoveCursorOverActiveSlot();
+
     RedrawWindow(g_programState.hWnd, NULL, NULL, RDW_INVALIDATE);
 }
 
@@ -855,6 +872,51 @@ static void QuitOverlay()
         if(IsIconic(hwnd)) {
             auto hr = ShowWindow(hwnd, SW_RESTORE);
             log(_T("restoring %p hr = %d\n"), (void*)hwnd, hr);
+        }
+
+        // I don't really need to do this since windows seem to sometimes do
+        // it automatically, but it's curtosy (or however you spell it)
+        
+        RECT r;
+        BOOL hrGWR = GetWindowRect(hwnd, &r);
+        POINT p;
+        BOOL hrGCP = GetCursorPos(&p);
+
+        auto& mis = monitorGeom;
+        if(hrGWR != FALSE && hrGCP != FALSE && !PtInRect(&r, p)) {
+            POINT tentativePoint = {
+                (r.right + r.left) / 2,
+                (r.bottom + r.top) / 2,
+            };
+            auto found = std::find_if(mis.monitors.begin(), mis.monitors.end(),
+                [&tentativePoint](MonitorInfo_t& mon) -> bool {
+                    return PtInRect(&mon.extent, tentativePoint) != FALSE;
+                });
+            if(found != mis.monitors.end()) {
+                log(_T("Moving the mouse centered on the window\n"));
+                SetCursorPos(tentativePoint.x, tentativePoint.y);
+            } else {
+                auto hMonitor = MonitorFromWindow(hwnd, MONITOR_DEFAULTTONEAREST);
+                MONITORINFO mi;
+                mi.cbSize = sizeof(MONITORINFO);
+                mi.dwFlags = 0;
+                GetMonitorInfo(hMonitor, &mi);
+                
+                RECT newTarget;
+                BOOL hrIR = IntersectRect(&newTarget, &mi.rcWork, &r);
+
+                if(hrIR) {
+                    log(_T("Moving the mouse centered on the intersection of the window and the monitor it's on\n"));
+                    SetCursorPos(
+                        (newTarget.left + newTarget.right) / 2,
+                        (newTarget.top + newTarget.bottom) / 2);
+                } else {
+                    log(_T("Moving the mouse centered on the monitor it's on\n"));
+                    SetCursorPos(
+                        (mi.rcWork.left + mi.rcWork.right) / 2,
+                        (mi.rcWork.top + mi.rcWork.bottom) / 2);
+                }
+            }
         }
     }
 }
@@ -911,6 +973,8 @@ static void ActivateSwitcher()
     g_programState.filter = _T("");
     CreateThumbnails(g_programState.filter);
     SetThumbnails();
+
+    MoveCursorOverActiveSlot();
 }
 
 static void ShowContextMenu(int x, int y)
@@ -1021,8 +1085,8 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         break;
     case WM_MOUSEWHEEL: {
         int amount = GET_WHEEL_DELTA_WPARAM(wParam);
-        if(amount > 0) MoveNext(VK_LEFT);
-        else MoveNext(VK_RIGHT);
+        if(amount > 0) MoveNext(VK_BACK);
+        else MoveNext(VK_TAB);
         break; }
     case WM_RBUTTONUP:
         if(!g_programState.showing) break;
