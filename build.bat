33c1e4ef6db54b79582ed769f031d5b0fbf751a3
alzwded <alzwded@gmail.com>
2016-04-18 19:55:52 +0300

    Add a convenient script to build the project
    ...because I'm too lazy to open VS and I always forget how to manually
    launch msbuild.

---
+++ build.bat
@@ -0,0 +1,25 @@
+@ECHO OFF
+
+IF #%1 == # GOTO :fullRebuild
+IF #%1 == #compile GOTO :compile
+IF #%1 == #clean GOTO :clean
+IF #%1 == #rebuild GOTO :fullRebuild
+GOTO :options
+
+:options
+ECHO Usage: %0 ^(compile^|clean^|rebuild^)
+EXIT 255
+
+:fullRebuild
+msbuild /property:Configuration=Release /property:Platform=x64 /target:Clean
+msbuild /property:Configuration=Release /property:Platform=Win32 /target:Clean
+GOTO :compile
+
+:compile
+msbuild /property:Configuration=Release /property:Platform=x64 && msbuild /property:Configuration=Release /property:Platform=Win32
+GOTO :EOF
+
+:clean
+msbuild /property:Configuration=Release /property:Platform=x64 /target:Clean
+msbuild /property:Configuration=Release /property:Platform=Win32 /target:Clean
+GOTO :EOF
